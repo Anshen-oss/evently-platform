b18b6b4bfce8a6eaeec77825248d9dd60e904f1e
Anshen-oss <plouisin@gmail.com>
2024-06-03 11:28:40 +0200

    first commit wit Stripe

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,6 @@
+import { SignUp } from "@clerk/nextjs";
+import { pages } from "next/dist/build/templates/app-page";
+
+export default function Page() {
+  return <SignUp />;
+}
---
+++ app/(root)/events/[id]/update/page.tsx
@@ -0,0 +1,40 @@
+import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/event.actions";
+import { UpdateEventParams } from "@/types";
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
+
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
+  const event = await getEventById(id);
+
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Update Event
+        </h3>
+      </section>
+
+      <div className="wrapper my-8">
+        <EventForm
+          event={event}
+          eventId={event._id}
+          userId={userId}
+          type="Update"
+        />
+      </div>
+    </>
+  );
+};
+
+export default UpdateEvent;
